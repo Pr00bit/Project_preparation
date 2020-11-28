3b81f0e877d82e939aab67dc41121ce944b6836f
Idzik <marcin.idzik@accenture.com>
2020-11-28 18:04:19 +0100

    All completed

--- Project_preparation.py
+++ Project_preparation.py
@@ -6,17 +6,28 @@ print("  ***   Welome in project creation tool  ***  ")
 print(' ')
 
 
-project_name = input('Please provide project name which should be created: ')
+project_name = input( '   Please provide project name which should be created: ')
+print(' ')
 
 # Creation of folder on drive
 os.system("mkdir C:\\Download\\Development\\" + project_name)
-print(' * Folder was created for new project')
+print(' *  1/3 Folder was created for new project')
 print(' ')
 
 
 # Creation of repo on Github
-g = Github('a5810f363d5c985a7a27e01133bea2f830e8610a')
+g = Github(' ea7f1cd79798a1245290ea37d372990a5f895799')
+
 user = g.get_user()
 repo = user.create_repo(project_name)
+ 
+print(' *  2/3 Remote repository was created on Github')
+print(' ')
 
-print(' * Remote repository was created on Github')
+# Cloning og repo from Github
+os.system("cd C:\\Download\\Development\\" + project_name)
+os.system("git clone https://github.com/Pr00bit/"+project_name+".git")
+
+print(' ')
+print(' *  3/3 Remote repository was cloned to local directory')
+print(' ')
