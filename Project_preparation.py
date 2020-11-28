e74bcb4fb3fee42b86cde4c607194b9b0e67d87e
Idzik <marcin.idzik@accenture.com>
2020-11-28 18:14:02 +0100

    All completed

--- Project_preparation.py
+++ Project_preparation.py
@@ -11,17 +11,17 @@ print(' ')
 
 # Creation of folder on drive
 os.system("mkdir C:\\Download\\Development\\" + project_name)
-print(' *  1/3 Folder was created for new project')
+print(' *  1/3 Folder was created for new project !')
 print(' ')
 
 
 # Creation of repo on Github
-g = Github(' ea7f1cd79798a1245290ea37d372990a5f895799')
+g = Github(' ')
 
 user = g.get_user()
 repo = user.create_repo(project_name)
  
-print(' *  2/3 Remote repository was created on Github')
+print(' *  2/3 Remote repository was created on Github !')
 print(' ')
 
 # Cloning og repo from Github
@@ -29,5 +29,16 @@ os.system("cd C:\\Download\\Development\\" + project_name)
 os.system("git clone https://github.com/Pr00bit/"+project_name+".git")
 
 print(' ')
-print(' *  3/3 Remote repository was cloned to local directory')
+print(' *  3/3 Remote repository was cloned to local directory !')
+print(' ')
+
+# Activation o"n virtualenv
+
+os.chdir("C:\\Download\\Development\\" + project_name)   # changing direcotry works better when use chdir not cd
+os.system("virtualenv .")
+
+
+
+print(' ')
+print(' *  4/4 virtualenv was instaled for the project !')
 print(' ')
